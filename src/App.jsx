10393b4f94385158d4bdfc26de6d2cf08df99923
Sumit-Kumar <sumit@frontendzip.com>
2024-05-17 14:20:38 +0530

    get the value of input box and store the value

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import ContainerTodo from "./Components/ContainerTodos/ContainerTodo"
 import InputBox from "./Components/InputData/InputBox"
 import Navbar from "./Components/Navbar"
-import TodoList from "./Components/Showtodos/TodoList"
-
 
 function App() {
 
--- src/Components/InputData/InputBox.jsx
+++ src/Components/InputData/InputBox.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import './InputBox.css'
 
 function InputBox(){
+    const [valueInput , setValueInput] = useState("")
+    
+    function getInputValue(){
+        console.log(valueInput);
+    }
+
     return (
         <div id="inputContainer">
-            <input type="text" placeholder='Enter your comment'/>
-            <button>Comment</button>
+            <input type="text" placeholder='Enter your comment' value={valueInput} onChange={(e)=>setValueInput(e.target.value)}/>
+            <button onClick={getInputValue}>Comment</button>
         </div>
     )
 }
