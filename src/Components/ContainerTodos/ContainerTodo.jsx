62952c76504a085f93836322d858bf1f9ad44114
Sumit kumar <Sumit@frontendzip.com>
2024-05-20 12:51:19 +0530

    Create and resolve InputBox Components

--- src/Components/ContainerTodos/ContainerTodo.jsx
+++ src/Components/ContainerTodos/ContainerTodo.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
-import TodoList from '../Showtodos/TodoList'
-import './ContainerTodo.css'
-function ContainerTodo({ items, classUpdate = false }) {
-    const [itemsState, setItemsState] = useState(items);
+// import { useState } from 'react';
+// import TodoList from '../Showtodos/TodoList'
+// import './ContainerTodo.css'
+// function ContainerTodo({ items, classUpdate = false, setItems, parentId }) {
+//     const [itemsState, setItemsState] = useState(items);
 
+//     return (
+//         <div id="ContainerTodos" style={{ margin: classUpdate ? "0px 0px 0px 100px" : "0px" }} key={Math.random()*100}>
+//             {itemsState.map((item) => (
+//                 <TodoList itemData={item} key={item.id} setItems={setItemsState} parentId={parentId} />
+//             ))}
+//         </div>
+//     );
+// }
 
-    return (
-        <div id="ContainerTodos" style={{ margin: classUpdate ? "0px 0px 0px 100px" : "0px" }}>
-            {itemsState.map((item) => (
-                <TodoList itemData={item} key={item.id} setItems={setItemsState} />
-            ))}
-        </div>
-    );
-}
-
-export default ContainerTodo;
+// export default ContainerTodo;
--- src/Components/InputData/InputBox.jsx
+++ src/Components/InputData/InputBox.jsx
@@ -1,37 +1,21 @@
 import { useState } from 'react';
 import './InputBox.css'
-import ContainerTodo from '../ContainerTodos/ContainerTodo';
 
-function InputBox({ classUpdate = false, id }) {
-    const [data, setData] = useState("");
-    const [items, setItems] = useState([])
+function InputBox() {
+    const [data, setData] = useState("")
 
-    function getInputValue() {
-        setItems((prevItems) =>
-            [...prevItems, {
-                id: Math.floor(Math.random() * 10000000),
-                value: data,
-                backComment: prevItems.length > 0 ? [...prevItems.map(item => item.value)] : []
-            }]
-        )
-        setData("");
-      
+    function handleData() {
+        setData(data)
     }
 
 
-    function handleKeyDown(event) {
-        if (event.key === 'Enter') {
-            getInputValue();
-        }
-    }
 
     return (
         <>
-            <div id="inputContainer" style={{ margin: classUpdate ? "0px 0px 0px 100px" : "0px" }}>
-                <input type="text" placeholder='Enter your comment' value={data} onChange={(e) => setData(e.target.value)} onKeyDown={handleKeyDown} />
-                <button onClick={() => getInputValue()}>Comment</button>
+            <div id="inputContainer" >
+                <input type="text" placeholder='Enter your comment' value={data} onChange={(e) => setData(e.target.value)} />
+                <button onClick={() => handleData()}>Comment</button>
             </div>
-            <ContainerTodo items={items} classUpdate={classUpdate} />
 
         </>
     )
